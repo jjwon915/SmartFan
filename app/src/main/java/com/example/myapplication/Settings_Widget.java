d40f914953c0bf7bb0c2efd3b7dfec5389e747c1
psh0121 <tngus2057@naver.com>
2020-07-18 01:45:48 +0900

    툴바 생성 및 환경설정의 리스트의 각 항목 재배치
    1) settings의 각 리스트들을 클릭시 각 화면으로 접속한 후 뒤로갈 수 있도록 툴바에 back버튼 적용
    
    2) settings_temperature화면에서 새로 적용한 툴바로 인해 깨진 배치구도를 재정비
    
    3) settings_advice인 도움말에서 '분류'정보를 확인할 수 있는 api공간 삭제

--- app/src/main/java/com/example/myapplication/Settings_Widget.java
+++ app/src/main/java/com/example/myapplication/Settings_Widget.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,23 @@ public class Settings_Widget extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings__widget);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null); // 화면에 보이는 앱 제목 타이틀을 제거
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
